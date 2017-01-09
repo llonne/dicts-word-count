b3149d45056baee68ee2da158d833731045368ee
Hackbright Student <no-reply@hackbrightacademy.com>
2017-01-09 12:50:08 -0800

    Added string.punctuation.

--- wordcount.py
+++ wordcount.py
@@ -1,3 +1,6 @@
+import string
+
+
 def count_words(words_file):
     """Takes a file and counts word occurrence."""
 
@@ -6,20 +9,24 @@ def count_words(words_file):
     all_words_list = []
 
     for line in words_list:
-        # phrase = words_string.append(line)
         line = line.rstrip()
+        # line = line.strip(string.punctuation)
+        # line.translate(None, string.punctuation)
         words_in_line = line.split(" ")
         for word in words_in_line:
-            all_words_list.append(word)
+            new_word = word.strip(string.punctuation)
+            all_words_list.append(new_word)
 
     words_count = {}
 
     for word in all_words_list:
         words_count[word] = words_count.get(word, 0) + 1
 
-    def get_count(word):
-        return words_count.get(word, 0)
+    # def get_count(word):
+    #     return words_count.get(word, 0)
 
     #print get_count("met")
+    for item, count in words_count.items():
+        print "%s, %d" % (item, count)
 
 count_words("test.txt")
